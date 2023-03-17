9ee5ca0c56bea9e99e1e4bc912a476269901e8b4
oloref <samping2money@gmail.com>
2023-03-17 19:34:32 +0700

    jquery not async

--- app/js/main.js
+++ app/js/main.js
@@ -1,81 +1,71 @@
-document.addEventListener('DOMContentLoaded', function() {
+$(function() {
 	
-    // counter up
-//     function VanillaCounter() {
-//         let elements = document.querySelectorAll('[data-vanilla-counter]')
-//         elements.forEach(i => {
-//             let data = {
-//                 startAt: parseInt(i.getAttribute('data-start-at')),
-//                 endAt: parseInt(i.getAttribute('data-end-at')),
-//                 delay: parseInt(i.getAttribute('data-delay')) || 0,
-//                 format: '{}',
-//                 time: parseInt(i.getAttribute('data-time')) || 0.1
-//             }
-//             if (i.getAttribute('data-format')) {
-//                 data.format = i.getAttribute('data-format')
-//             } else if (i.innerHTML != "") {
-//                 data.format = i.innerHTML
-//             }
-//             console.log(data.format)
-//             if (data.startAt == null) {
-//                 throw new Error('data-start-at attribute is required')
-//             }
-//             if (data.endAt == null) {
-//                 throw new Error('data-end-at attribute is required')
-//             }
-//             var counter = data.startAt
-//             i.innerHTML = data.format.replace('{}', counter)
-//             var intervalTime = Math.ceil(data.time / (data.endAt - data.startAt))
-//             setTimeout(() => {
-//                 var interval = setInterval(intervalHandler, intervalTime)
-//                 function intervalHandler() {
-//                     counter += (data.endAt - data.startAt) / Math.abs(data.endAt - data.startAt) * 1
-//                     i.innerHTML = data.format.replace('{}', counter)
-//                     if (counter == data.endAt) {
-//                         clearInterval(interval)
-//                     }
-//                 }
-//             }, data.delay)
-//         })
-//     }
-//    // Получаем нужный элемент
-//     var element = document.querySelector('.numbers__box');
-//         var Visible = function (target) {
-//         // Все позиции элемента
-//         var targetPosition = {
-//             top: window.pageYOffset + target.getBoundingClientRect().top,
-//             left: window.pageXOffset + target.getBoundingClientRect().left,
-//             right: window.pageXOffset + target.getBoundingClientRect().right,
-//             bottom: window.pageYOffset + target.getBoundingClientRect().bottom
-//             },
-//             // Получаем позиции окна
-//             windowPosition = {
-//             top: window.pageYOffset,
-//             left: window.pageXOffset,
-//             right: window.pageXOffset + document.documentElement.clientWidth,
-//             bottom: window.pageYOffset + document.documentElement.clientHeight
-//             };
 
-//         if (targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
-//             targetPosition.top < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
-//             targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
-//             targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
-//             // Если элемент полностью видно, то запускаем следующий код
-//             console.clear();
-//             //console.log('Вы видите элемент :)');
-//             VanillaCounter();
-//         } else {
-//             // Если элемент не видно, то запускаем этот код
-//             //console.clear();
-//         };
-//     };
-//     // Запускаем функцию при прокрутке страницы
-//     window.addEventListener('scroll', function() {
-//         Visible(element);
-//     });
-    
 
-    
+    //Ymap start
+    var spinner = $('.ymap-container').children('.loader');
+    var check_if_load = 0;
+    var myMap, myPlacemark;
+
+    function init() {
+        myMap = new ymaps.Map('map', {
+            center: [55.779003, 37.629475],
+            zoom: 16,
+            controls: []
+        });
+        // myMap.behaviors.disable('scrollZoom');
+        // myMap.behaviors.disable('drag');
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+            myPlacemark = new ymaps.Placemark([55.779003, 37.629475], {
+                hintContent: 'г. Москва, ул. Щепкина, д.28, офис 413',
+                balloonContent: 'г. Москва, ул. Щепкина, д.28, офис 413'
+            }, {
+                iconLayout: 'default#image',
+                iconImageHref: 'img/map.webp',
+                iconImageSize: [62, 62],
+                iconImageOffset: [-15, -42]
+            });
+        var zoomControl = new ymaps.control.ZoomControl({
+            options: {
+                size: "auto",
+                position: {
+                    top: 120,
+                    left: 15
+                }
+            }
+        });
+        myMap.controls.add(zoomControl);
+        myMap.geoObjects.add(myPlacemark);
+
+        //Получаем первый экземпляр коллекции слоев, потом первый слой коллекции
+        var layer = myMap.layers.get(0).get(0);
+        //Решение по callback-у для определния полной загрузки карты: http://ru.stackoverflow.com/questions/463638/callback-загрузки-карты-yandex-map
+        waitForTilesLoad(layer).then(function () {
+            //Скрываем
+            spinner.removeClass('is-active')
+        });
+    };
+
+    function waitForTilesLoad(layer) {
+        return new ymaps.vow.Promise(function (resolve, reject) {
+            var tc = getTileContainer(layer),
+                readyAll = true;
+            tc.tiles.each(function (tile, number) {
+                if (!tile.isReady()) {
+                    readyAll = false;
+                }
+            });
+            if (readyAll) {
+                resolve();
+            } else {
+                tc.events.once("ready", function () {
+                    resolve();
+                });
+            }
+        });
+    }
     
     
     
