39bf56f3833b825a0090b4d86812057c5ffdafa6
oloref <samping2money@gmail.com>
2023-03-19 14:53:49 +0700

    select fixed q

--- app/js/main.js
+++ app/js/main.js
@@ -74,14 +74,7 @@ $(document).ready(function() {
         })
         detectmob(mobileFn, desctopFn)
         function detectmob(x, y) {
-            if (navigator.userAgent.match(/Android/i)
-                || navigator.userAgent.match(/webOS/i)
-                || navigator.userAgent.match(/iPhone/i)
-                || navigator.userAgent.match(/iPad/i)
-                || navigator.userAgent.match(/iPod/i)
-                || navigator.userAgent.match(/BlackBerry/i)
-                || navigator.userAgent.match(/Windows Phone/i)
-            ) {
+            if (0 == 1) {
                 x();
             }
             else {
